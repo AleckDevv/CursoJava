9e4d68c87fbad5c58b85732bfec65a926851f3e2
Alex Soares Lopes <as3434126@gmail.com>
2025-09-17 10:34:26 -0300

    add arquivo sobre os tipos de variáveis

---
+++ src/com/alex/cursojava/exercicios/Variaveis.java
@@ -0,0 +1,17 @@
+package com.alex.cursojava.exercicios;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        // tipos de variáveis no java
+        String nome;
+        int idade;
+        float pi;
+        double peso;
+        char letra;
+        boolean verdadeiro;
+        boolean falso;
+        byte zero;
+        short cem;
+        long numeroGrandeComLNoFinal;
+    }
+}
